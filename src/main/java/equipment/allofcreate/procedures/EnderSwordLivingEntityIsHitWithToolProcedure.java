dfc259a241a90c5c8b24c4d1af6a7ce9bd19cc0b
Бобрик Павел <promas0234@gmail.com>
2026-08-10 02:15:22 +0300

    Update EnderSwordLivingEntityIsHitWithToolProcedure.java

--- src/main/java/equipment/allofcreate/procedures/EnderSwordLivingEntityIsHitWithToolProcedure.java
+++ src/main/java/equipment/allofcreate/procedures/EnderSwordLivingEntityIsHitWithToolProcedure.java
@@ -13,10 +13,10 @@ public class EnderSwordLivingEntityIsHitWithToolProcedure {
 			_entity.addEffect(new MobEffectInstance(MobEffects.WEAKNESS, 120, 1, false, false));
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 			_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 120, 1, false, false));
- if (sourceentity instanceof net.minecraft.world.entity.player.Player _player && _player.getInventory().getArmor(3).getItem() == equipment.allofcreate.item.EnderArmorItem.HELMET.get()
-                && _player.getInventory().getArmor(2).getItem() == equipment.allofcreate.item.EnderArmorItem.CHESTPLATE.get()
-                && _player.getInventory().getArmor(1).getItem() == equipment.allofcreate.item.EnderArmorItem.LEGGINGS.get()
-                && _player.getInventory().getArmor(0).getItem() == equipment.allofcreate.item.EnderArmorItem.BOOTS.get()) {
+ if (sourceentity instanceof net.minecraft.world.entity.player.Player _player && _player.getInventory().getArmor(3).getItem() == equipment.allofcreate.item.EnderArmorItem.Helmet.get()
+                && _player.getInventory().getArmor(2).getItem() == equipment.allofcreate.item.EnderArmorItem.Chestplate.get()
+                && _player.getInventory().getArmor(1).getItem() == equipment.allofcreate.item.EnderArmorItem.Leggings.get()
+                && _player.getInventory().getArmor(0).getItem() == equipment.allofcreate.item.EnderArmorItem.Boots.get()) {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 120, 0, false, false));
 		}
