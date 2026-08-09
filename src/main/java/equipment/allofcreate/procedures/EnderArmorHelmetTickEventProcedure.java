84ccb4a5b66e0d287b05d77c32dc146e9cf70d56
Бобрик Павел <promas0234@gmail.com>
2026-08-10 02:36:44 +0300

    Update EnderArmorHelmetTickEventProcedure.java

--- src/main/java/equipment/allofcreate/procedures/EnderArmorHelmetTickEventProcedure.java
+++ src/main/java/equipment/allofcreate/procedures/EnderArmorHelmetTickEventProcedure.java
@@ -10,10 +10,10 @@ public class EnderArmorHelmetTickEventProcedure {
 		if (entity == null)
 			return;
 if (sourceentity instanceof net.minecraft.world.entity.player.Player _player 
-    && _player.getInventory().getArmor(3).getItem() == equipment.allofcreate.allOfCreateEquipment.HELMET.get()
-    && _player.getInventory().getArmor(2).getItem() == equipment.allofcreate.allOfCreateEquipment.CHESTPLATE.get()
-    && _player.getInventory().getArmor(1).getItem() == equipment.allofcreate.allOfCreateEquipment.LEGGINGS.get()
-    && _player.getInventory().getArmor(0).getItem() == equipment.allofcreate.allOfCreateEquipment.BOOTS.get()) {
+                && _player.getInventory().getArmor(3).getItem() == equipment.allofcreate.allOfCreateEquipment.HELMET.get()
+                && _player.getInventory().getArmor(2).getItem() == equipment.allofcreate.allOfCreateEquipment.CHESTPLATE.get()
+                && _player.getInventory().getArmor(1).getItem() == equipment.allofcreate.allOfCreateEquipment.LEGGINGS.get()
+                && _player.getInventory().getArmor(0).getItem() == equipment.allofcreate.allOfCreateEquipment.BOOTS.get()) {
 			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 				_entity.addEffect(new MobEffectInstance(MobEffects.NIGHT_VISION, 40, 1, false, false));
 		} else {
