505ed2f6c2996eff1c120025b90c348a2fcea0e0
Бобрик Павел <promas0234@gmail.com>
2026-08-10 02:24:45 +0300

    Update allOfCreateEquipment.java

--- src/main/java/equipment/allofcreate/allOfCreateEquipment.java
+++ src/main/java/equipment/allofcreate/allOfCreateEquipment.java
@@ -73,3 +73,7 @@ public class allOfCreateEquipment {
         }
     }
 }
+    public static final DeferredHolder<Item, Item> HELMET = ENDER_ARMOR_HELMET;
+    public static final DeferredHolder<Item, Item> CHESTPLATE = ENDER_ARMOR_CHESTPLATE;
+    public static final DeferredHolder<Item, Item> LEGGINGS = ENDER_ARMOR_LEGGINGS;
+    public static final DeferredHolder<Item, Item> BOOTS = ENDER_ARMOR_BOOTS;
